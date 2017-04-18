fd25ac558f1332c497ca1dd928d5b22786a3e710
zhangwei <784623601@qq.com>
2017-04-18 09:50:15 +0800

    '完成task动态数据绑定3'

---
+++ ife/advanceJsTask4/js/Observer.js
@@ -0,0 +1,73 @@
+/**
+ * Created by 78462 on 2017/4/17.
+ */
+(function(gloabal) {
+
+    'use strict';
+
+    class Observer {
+        constructor(data) {
+            this.data = data;
+            this.hijack(data);
+            this.$eventer = new Eventer();
+        }
+
+        watch(obj, propName, init, objName) {
+            let _this = this;
+            this.hijack(init, propName);
+            Object.defineProperty(obj, propName, {
+                get() {
+                    return init;
+                },
+
+                set(newValue) {
+                    _this.hijack(newValue);
+                    if (objName !== undefined) {
+                        _this.$eventer.emit(objName + '.' + propName, newValue);
+                    } else {
+                        _this.$eventer.emit(propName, newValue);
+                    }
+                    init = newValue;
+                }
+            });
+        }
+
+        $watch(propName, cb) {
+            this.$eventer.on(propName, cb);
+        }
+
+        hijack(data, dataName) {
+            if (typeof data === 'object') {
+                for (let key in data) {
+                    this.watch(data, key, data[key], dataName);
+                }
+            }
+        }
+    }
+
+    class Eventer {
+        constructor() {
+            this.events = {}
+        }
+
+        on(eventName, cb) {
+            this.events[eventName] = this.events[eventName] || [];
+            this.events[eventName].push(cb);
+        }
+
+        emit(eventName, data) {
+            let values = eventName.split('.'),
+                arr = this.events[eventName];
+            if (Array.isArray(arr)) {
+                arr.forEach(cb => cb(data));
+            }
+            if (values.length > 1) {
+                values.pop();
+                this.emit(values.join('.'), data);
+            }
+        }
+    }
+
+    gloabal.Observer = Observer;
+
+})(this);
