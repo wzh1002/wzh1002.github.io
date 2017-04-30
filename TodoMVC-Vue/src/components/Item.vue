04ba9d3f44bedbde9e083af97db351a27123de32
zhangwei <784623601@qq.com>
2017-04-30 21:09:17 +0800

    '新增TodoMVC-Vue'

---
+++ TodoMVC-Vue/src/components/Item.vue
@@ -0,0 +1,31 @@
+<template>
+    <li :class="{ completed: todo.completed, editing: editing === todo}">
+        <div class="view">
+            <input class="toggle" type="checkbox" v-model="todo.completed">
+            <label @dblclick="editTodo(todo)">{{ todo.title }}</label>
+            <button class="destroy" @click="removeTodo(todo)"></button>
+        </div>
+        <input class="edit" type="text" v-model="todo.title" @blur="doneEdit(todo)" @keyup.enter="doneEdit(todo)" @keyup.esc="cancelEdit(todo)">
+    </li>
+</template>
+<script type="text/ecmascript-6">
+    import bus from '../bus';
+
+    export default {
+        props: ['todo', 'editing'],
+        methods: {
+            editTodo: function(todo) {
+                bus.$emit('editTodo', todo);
+            },
+            doneEdit: function(todo) {
+                bus.$emit('doneEdit', todo);
+            },
+            cancelEdit: function(todo) {
+                bus.$emit('cancelEdit', todo);
+            },
+            removeTodo: function(todo) {
+                bus.$emit('removeTodo', todo)
+            }
+        }
+    }
+</script>
